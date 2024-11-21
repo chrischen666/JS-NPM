4c62126ee05a0c88f2bad3396da963327beab188
chris chen <kjh88126@gmail.com>
2024-11-22 00:22:03 +0800

    123

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                about: 'about.html', // 新增其他頁面的 HTML 文件
+            },
+        },
+    },
     base: '/JS-NPM/'
-}
+});
