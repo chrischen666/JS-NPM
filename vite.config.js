adfeb3389a892dc341b1780e566a17cf6480f23f
chris chen <kjh88126@gmail.com>
2024-11-22 13:29:43 +0800

    123

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/JS-NPM/',
+    base: '/JS-NPM/', // 部署路徑
     build: {
         rollupOptions: {
             input: {
                 main: 'index.html',
                 admin: 'admin.html'
+            },
+            external: ['d3', 'c3'], // 防止重新打包這些模組
+            output: {
+                manualChunks: {
+                    d3: ['d3'], // 分離 d3
+                    c3: ['c3']  // 分離 c3
+                }
             }
         }
     },
     optimizeDeps: {
-        include: ['d3', 'c3']
+        include: ['d3', 'c3'] // 加速開發環境
     }
-})
+});
