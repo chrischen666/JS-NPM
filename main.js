6e7476870d73276e63b6c42a099a59847bd06cf4
chris chen <kjh88126@gmail.com>
2024-11-22 13:59:57 +0800

    1213

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 
+// 同時要引入 CSS
 import "./css/app.css";
+// import './js/d3.js';
+// import './js/c3.js';
 import './js/d3.js';
 import './js/c3.js';
 import './js/admin.js';
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/JS-NPM/', // 部署路徑
+    base: '/JS-NPM/',
     build: {
         rollupOptions: {
             input: {
                 main: 'index.html',
                 admin: 'admin.html'
             },
-            external: ['d3', 'c3'], // 防止重新打包這些模組
             output: {
                 manualChunks: {
-                    d3: ['d3'], // 分離 d3
-                    c3: ['c3']  // 分離 c3
+                    d3Chunk: ['d3'],
+                    c3Chunk: ['c3']
                 }
             }
         }
     },
     optimizeDeps: {
-        include: ['d3', 'c3'] // 加速開發環境
+        include: ['d3', 'c3']
     }
 });
